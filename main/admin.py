e2ba6cb48c52f53409dc229956fcab9f0c04c545
T30M47 <119812961+T30M47@users.noreply.github.com>
2023-12-18 18:57:14 +0100

    Delete main directory

--- main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-modeli = [Proizvod_SIndeksom, Proizvod_bezIndeksa, Proizvod_DioIndeksa, Proizvod_KriviIndeks, Proizvod_bezIndeksa_MaloRedaka, Proizvod_SIndeksom_MaloRedaka, Proizvod_SIndeksom_VelikaKard, Proizvod_DioIndeksa_VelikaKard, Proizvod_KriviIndeks_VelikaKard, Proizvod_SIndeksom_Datum, Proizvod_SIndeksom_VelikaKard_Datum]
-
-admin.site.register(modeli)
